b33ab20565ecd0b5929e08ad3bbe4190b93872f7
wziyue020 <wziyue020@163.com>
2015-01-23 13:28:16 +0800

    3.0

--- MySQL-03-01.sql
+++ MySQL-03-01.sql
@@ -13,7 +13,8 @@ CREATE TABLE employee
 (
   id      INT(10) PRIMARY KEY,
   name    CHAR(20),
-  phone   INT(12),
+  salary    INT(10),
+  phone   INT(12) NOT NULL,
   in_dpt  CHAR(20) NOT NULL,
   UNIQUE  (phone),
   CONSTRAINT emp_fk FOREIGN KEY (in_dpt) REFERENCES department(dpt_name)
