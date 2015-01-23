247ccdc3ce6d25b70c23cbe11727e77ac1db3f0f
wziyue020 <wziyue020@163.com>
2015-01-23 11:56:13 +0800

    2.0

--- MySQL-03-01.sql
+++ MySQL-03-01.sql
@@ -1,8 +1,12 @@
+CREATE DATABASE mysql_shiyan;
+
+use mysql_shiyan;
+
 CREATE TABLE department
 (
   dpt_name   CHAR(20) NOT NULL,
   people_num INT(10) DEFAULT '10',
-¡¡CONSTRAINT dpt_pk PRIMARY KEY (dpt_name)
+  CONSTRAINT dpt_pk PRIMARY KEY (dpt_name)
  );
 
 CREATE TABLE employee
@@ -20,7 +24,7 @@ CREATE TABLE project
   proj_num   INT(10) NOT NULL,
   proj_name  CHAR(20) NOT NULL,
   start_date DATE NOT NULL,
-  end_date   DATE DEFAULT '2015-04-01'£¬
-  of_dpt     CHAR(20) REFERENCES department(dpt_name)£¬
+  end_date   DATE DEFAULT '2015-04-01',
+  of_dpt     CHAR(20) REFERENCES department(dpt_name),
   CONSTRAINT proj_pk PRIMARY KEY (proj_num,proj_name)
  );
